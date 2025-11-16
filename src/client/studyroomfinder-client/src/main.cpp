f51df815a746947f73adb65ca9ac30cc5233c77c
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-11-15 21:29:19 -0500

    measure sound amplitude from microphone

--- src/client/studyroomfinder-client/src/main.cpp
+++ src/client/studyroomfinder-client/src/main.cpp
@@ -1,15 +1,37 @@
 #include <Arduino.h>
 
-const int microphonePin = A0;
+const int micPin = A0;
+const int sampleMillis = 50;
 
 void setup() {
-  pinMode(microphonePin, INPUT);
   Serial.begin(115200);
+  pinMode(micPin, INPUT);
 }
 
 void loop() {
-  Serial.print("volume level: ");
-  int val = analogRead(microphonePin);
-  Serial.println(val);
-  delay(100);
+  unsigned long startTime = millis();
+  unsigned int volume = 0;
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1023;
+  unsigned int currSample;
+
+  while (millis() - startTime < sampleMillis) {
+    currSample = analogRead(A0);
+    if (currSample < 1023) {
+      if (currSample > signalMax) {
+        signalMax = currSample;
+      }
+      if (currSample < signalMin) {
+        signalMin = currSample;
+      }
+    }
+  }
+  
+  volume = signalMax - signalMin;
+
+  Serial.print("Sound Amplitude: ");
+  for (unsigned int i=0;i<volume;i+=2)
+    Serial.print(".");
+  Serial.print(volume);
+  Serial.println();
 }
